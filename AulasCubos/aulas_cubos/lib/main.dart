d0828d500396acbbce0bae6ac05690e4d072787c
Clovis Alves <clovisalvesjr@gmail.com>
2021-04-25 13:22:24 -0300

    Final Exercício

--- AulasCubos/aulas_cubos/lib/main.dart
+++ AulasCubos/aulas_cubos/lib/main.dart
@@ -9,9 +9,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: (Container(
-        color: Colors.blue,
-      )),
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(children: [
+            Column(
+              children: [
+                Container(
+                    color: Colors.pink,
+                    height: 200,
+                    width: 185,
+                    margin: EdgeInsets.fromLTRB(20, 0, 0, 0)),
+                Container(
+                    color: Colors.orange,
+                    height: 200,
+                    width: 185,
+                    margin: EdgeInsets.fromLTRB(20, 0, 0, 0))
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                    color: Colors.blueAccent,
+                    height: 100,
+                    width: 185,
+                    margin: EdgeInsets.fromLTRB(0, 0, 20, 0)),
+                Row(children: [
+                  Container(color: Colors.green, height: 300, width: 92),
+                  Container(
+                      color: Colors.grey,
+                      height: 300,
+                      width: 92,
+                      margin: EdgeInsets.fromLTRB(0, 0, 20, 0))
+                ]),
+              ],
+            ),
+          ]),
+          Row(
+            children: [
+              Container(
+                  color: Colors.red,
+                  height: 100,
+                  width: 300,
+                  margin: EdgeInsets.fromLTRB(20, 0, 0, 0)),
+              Container(
+                  color: Colors.blue,
+                  height: 100,
+                  width: 70,
+                  margin: EdgeInsets.fromLTRB(0, 0, 20, 0))
+            ],
+          ),
+          Container(
+            color: Colors.yellow,
+            height: 100,
+            width: 400,
+            margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+            alignment: Alignment.bottomCenter,
+          ),
+        ],
+      ),
     );
   }
 }
