b8db92c6b2bfd8d4557be1f386f0551cc2464dc2
Clovis Alves <clovisalvesjr@gmail.com>
2021-04-25 16:46:10 -0300

    Final Exercício

--- AulasCubos/aulas_cubos/lib/main.dart
+++ AulasCubos/aulas_cubos/lib/main.dart
@@ -4,14 +4,65 @@ void main() {
   runApp(MyApp());
 }
 
-cvoid main() {
-  runApp(MyApp());
-}
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+        home: Column(
+      children: [
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.all(50),
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 30),
+                  color: Colors.white,
+                  height: 100,
+                  width: 300,
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 30),
+                  color: Colors.white,
+                  height: 100,
+                  width: 300,
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 30),
+                  color: Colors.white,
+                  height: 100,
+                  width: 300,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          color: Colors.blue,
+          height: 100,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                color: Colors.green,
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Colors.yellow,
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Colors.red,
+                height: 50,
+                width: 50,
+              ),
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
